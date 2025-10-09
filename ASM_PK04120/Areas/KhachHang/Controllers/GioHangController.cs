86c5a1ce86ec54e3d28190f1ad876488bbda272c
khoi1 <84061502+khoi1@users.noreply.github.com>
2025-10-09 21:41:32 +0700

    Thêm giao diện giỏ hàng

---
+++ ASM_PK04120/Areas/KhachHang/Controllers/GioHangController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASM_PK04120.Areas.KhachHang.Controllers
+{
+    [Area("KhachHang")]
+    public class GioHangController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
