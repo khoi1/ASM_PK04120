917770e3c22ed9ea25fd12bb6d22dbd0990ee0bd
khoi1 <84061502+khoi1@users.noreply.github.com>
2025-10-10 21:28:48 +0700

    sua tim kiem

--- ASM_PK04120/Areas/KhachHang/Controllers/SanPhamController.cs
+++ ASM_PK04120/Areas/KhachHang/Controllers/SanPhamController.cs
@@ -6,19 +6,20 @@ namespace ASM_PK04120.Areas.KhachHang.Controllers
     [Area("KhachHang")]
     public class SanPhamController : Controller
     {
-
         private readonly ISanPhamService _sanPhamService;
         public SanPhamController(ISanPhamService sanPhamService)
         {
             _sanPhamService = sanPhamService;
         }
 
+        // Action này dùng để tải trang sản phẩm
         public async Task<IActionResult> SanPham(int? maDanhMuc, string? phamViGia, string? thuTuSapXep, int trang = 1)
         {
             var sp = await _sanPhamService.LayDanhSachSanPhamAsync(maDanhMuc, phamViGia, thuTuSapXep, trang);
             return View(sp);
         }
 
+        // Action cho trang chi tiết sản phẩm
         public async Task<IActionResult> ChiTietSanPham(int id)
         {
             var viewModel = await _sanPhamService.LayChiTietSanPhamAsync(id);
@@ -29,10 +30,10 @@ namespace ASM_PK04120.Areas.KhachHang.Controllers
             return View(viewModel);
         }
 
+        // Action cho TÌM KIẾM AJAX TRÊN HEADER
         [HttpGet]
         public async Task<JsonResult> TimKiemSanPham(string tuKhoa)
         {
-            // Gọi phương thức TimKiemSanPhamAsync đã được sửa đổi
             var result = await _sanPhamService.TimKiemSanPhamAsync(tuKhoa);
             return Json(result);
         }
